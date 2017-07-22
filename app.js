3e3d3c86e5c4d46d5706ac88f978f9249e49f6dc
= <=>
2017-07-22 17:16:24 -0400

    finished get and post data

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ app.get("/",function(req,res){
 	})
 })
 
+app.post("/status/new",function(req,res){
+	let status = JSON.stringify({"name":req['body']['name'],"status": req['body']['status']});
+	fs.writeFile(__dirname + "/posts.json",status,function(err){
+		if(err)console.log(err);
+	})
+
+})
+
+app.get("/status",function(req,res){
+	fs.readFile(__dirname + '/posts.json',function(err,data){
+		if(!err){
+			res.send(JSON.parse(data));
+		}else{
+			console.log(err);
+		}
+	})
+})
 
 app.listen(3000,function(err){
 	if(err){
--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.onload = function(){
 	console.log('dom content loaded');
+
+	fetch("/status").then(function(res){
+		res.json().then(function(data){
+			console.log(data)
+			document.getElementById('my-name').innerHTML = data['name'];
+			document.getElementById('my-status').innerHTML = data['status'];
+		})
+	})
+
 }
